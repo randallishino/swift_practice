ae2f65f4210af519c26779958aca2e997188952e
randall ishino <randallishino@gmail.com>
2018-10-07 16:12:45 -0700

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by randall ishino on 10/3/18.
+//  Copyright © 2018 randall ishino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
